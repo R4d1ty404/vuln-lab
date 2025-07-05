d704a765d46cf657b1317384df5c1c316a9d089e
Raditya Perwira Putra <raditpp315@gmail.com>
2025-07-05 23:43:49 +0700

    fix

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "phpmyadmin";
-$password = "thisisend07";
-$dbname = "project";
+$password = "";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
